3931a6cd0978cdf730c94c466ff4260a9569ad61
Eli Ennab <elinaennab@gmail.com>
2023-01-10 11:10:59 +0100

    Paths with __dirname and __filename.

--- 01-basics/01-globals.js
+++ 01-basics/01-globals.js
@@ -4,7 +4,7 @@
 * avbryt i Node med ctrl+c
 */
 
-console.log('Hello, world!')
+// console.log('Hello, world!')
 
 /*
 setTimeout(() => {
@@ -13,8 +13,20 @@ setTimeout(() => {
 */
 
 // Will continue running until cancelled ( ctrl + c )
+/*
 setInterval(() => {
 	console.log('Hello')
 }, 2000)
+*/
 
-console.log('Hello again, world!')
+// console.log('Hello again, world!')
+
+/*
+* Paths
+*/
+
+// The absolute path
+console.log("Absolute path to directory for this file:", __dirname)
+
+// The absolute path to this file
+console.log("The absolute path to this file:", __filename)
