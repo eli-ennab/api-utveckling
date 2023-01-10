f73015879b12ce02d866ad86a77adfe617f27b4c
Eli Ennab <elinaennab@gmail.com>
2023-01-10 14:06:18 +0100

    First Express app, listening to port 3000.

--- 02-express/server.js
+++ 02-express/server.js
@@ -2,4 +2,18 @@
 * Express Server
 */
 
-console.log('*little racing car*')
+// Require Express
+const express = require('express')
+
+// Create a port
+const PORT = 3000
+
+// Create a new Express app
+const app = express()
+
+// Start listening for incoming requests on port 3000
+app.listen(PORT, () => {
+	console.log(`Server started on localhost: ${PORT}`)
+})
+
+
