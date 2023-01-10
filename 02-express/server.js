89f93f22e2f2a503001f3158e6091027cc39ee2b
Eli Ennab <elinaennab@gmail.com>
2023-01-10 14:12:40 +0100

    GET / and GET /coffee

--- 02-express/server.js
+++ 02-express/server.js
@@ -11,6 +11,16 @@ const PORT = 3000
 // Create a new Express app
 const app = express()
 
+// GET /
+app.get(('/'), (req, res) => {
+	res.send("Hello, world...")
+})
+
+// GET /coffee
+app.get(('/coffee'), (req, res) => {
+	res.send("is good for you...")
+})
+
 // Start listening for incoming requests on port 3000
 app.listen(PORT, () => {
 	console.log(`Server started on localhost: ${PORT}`)
