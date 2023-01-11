bbf6145a2b9ac639bf8e1ca82e1f0ea00c18e391
Eli Ennab <elinaennab@gmail.com>
2023-01-11 12:08:18 +0100

    A random guess sent to the DOM. Workshop finished!

--- workshop-01/jokes.js
+++ workshop-01/jokes.js
@@ -7,20 +7,26 @@ const PORT = 3000
 // Create a new Express app
 const app = express()
 
+// Define filesystem
 const fs = require('fs');
 
-let joke
+// All jokes
+let jokes
 
+// A random joke
+let randomJoke
+
+// Reading oneliners
 fs.readFile('data/oneliners.json', (err, data) => {
-  if (err) throw err;
-  joke = JSON.parse(data);
-//   console.log(joke)
+	if (err) throw err;
+	jokes = JSON.parse(data);
+	console.log(jokes)
 
-  // Generate a random number between 0 and the last index of the array
-	const randomIndex = Math.floor(Math.random() * joke.length);
+	// Generate a random number between 0 and the last index of the array
+	const randomIndex = Math.floor(Math.random() * jokes.length);
 
-	// Get the joke at the randomly generated index
-	const randomJoke = joke[randomIndex];
+	// Get the jokes at the randomly generated index
+	randomJoke = jokes[randomIndex];
 
 	console.log(randomJoke);
 });
@@ -28,7 +34,7 @@ fs.readFile('data/oneliners.json', (err, data) => {
 // GET /
 app.get(('/'), (req, res) => {
 	res.send({
-		"joke": joke,
+		"joke": randomJoke,
 	})
 })
 
