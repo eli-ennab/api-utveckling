9d32725201e220f97ec4fc9bb3b94e8ade974e2c
Eli Ennab <elinaennab@gmail.com>
2023-01-17 11:05:39 +0100

    Lesson 4: mysql and dotenv.

---
+++ 03-mysql/server.js
@@ -0,0 +1,37 @@
+/**
+ * Express Server
+ */
+
+// Require stuff
+const express = require('express')
+const _ = require('lodash')
+const morgan = require('morgan')
+const PORT = 3000
+
+// Create a new Express app
+const app = express()
+
+// Parse any incoming JSON
+app.use(express.json())
+
+// Log information about all incoming requests using morgan
+app.use(morgan('dev'))
+
+// GET /
+app.get('/', (req, res) => {
+	res.send({
+		message: "Oh, hi there ☺️",
+	})
+})
+
+// Catch requests where a route does not exist
+app.use((req, res) => {
+	res.status(404).send({
+		message: `Sorry, no route exists for ${req.method} ${req.path}`,
+	})
+})
+
+// Start listening for incoming requests on port 3000
+app.listen(PORT, () => {
+	console.log(`🥳 Yay, server started on localhost:${PORT}`)
+})
