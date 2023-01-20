b146167ad9d7a1e80c492aafcb11768df14dd891
Eli Ennab <elinaennab@gmail.com>
2023-01-20 09:42:27 +0100

    Fix bug with releaseDate, still bugs in it tho.

--- 03-mysql/server.js
+++ 03-mysql/server.js
@@ -123,8 +123,9 @@ app.post('/movies', async (req, res) => {
 		return
 	}
 
+	// ! known bugs !
 	const releaseDate = new Date(release_date)
-	if (!releaseDate instanceof Date || isNaN(releaseDate)) {
+	if (release_date && (!releaseDate instanceof Date || isNaN(releaseDate)) ) {
 		res.status(400).send({
 			message: "release_date has to be a valid date."
 		})
