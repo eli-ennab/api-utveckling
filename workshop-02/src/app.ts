08f2f20f6ec1c610367f9d88989b708b3825d190
Eli Ennab <elinaennab@gmail.com>
2023-01-24 09:56:55 +0100

    Get all /users + /phones + try catch on status 500.

--- workshop-02/src/app.ts
+++ workshop-02/src/app.ts
@@ -15,9 +15,33 @@ app.use(morgan('dev'))
 // 	})
 // })
 
+// GET /users, all users
 app.get('/users', async (req, res) => {
-	const users = await prisma.users.findMany()
-	res.send(users)
+	try {
+		const users = await prisma.users.findMany()
+		res.send(users)
+	} catch (err) {
+		console.error(err)
+		res.status(500).send ( { message: "Something went wrong querying the database. Internal server error."})
+	}
 })
+
+// GET /phones, all phones
+app.get('/phones', async (req, res) => {
+	const phones = await prisma.phones.findMany()
+	res.send(phones)
+})
+
+// // GET one user
+// app.get('/users/:id', async (req, res) => {
+// 	const users = await prisma.users.findMany()
+// 	res.send(users)
+// })
+
+// // GET one phone
+// app.get('/phones/:id', async (req, res) => {
+// 	const phones = await prisma.phones.findMany()
+// 	res.send(phones)
+// })
 
 export default app
