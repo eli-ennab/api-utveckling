275b623f682e28e9027feb6b2bb60384e365dcb0
Eli Ennab <elinaennab@gmail.com>
2023-01-27 15:05:33 +0100

    Moved logic for authors + books to controllers.

--- 05-prisma-books/src/routes/authors.ts
+++ 05-prisma-books/src/routes/authors.ts
@@ -1,61 +1,15 @@
-// Handle all `/authors` routes
+// Handles all `/authors` routes
 import express from 'express'
-import prisma from '../prisma'
+import { index, store, addBook } from '../controllers/author_controller'
 const router = express.Router()
 
 // GET /authors
-router.get('/', async (req, res) => {
-	try {
-		const authors = await prisma.author.findMany({
-			include: {
-				books: true,
-			}
-		})
-		res.send(authors)
-	} catch {
-		res.status(500).send({ message: "Something went wrong." })
-	}
-})
+router.get('/', index)
 
 // POST /authors
-router.post('/', async (req, res) => {
-	const birthdate = (new Date(req.body.birthdate)).toISOString()
-
-	try {
-		const author = await prisma.author.create({
-			data: {
-				name: req.body.name,
-				birthdate: birthdate,
-			}
-		})
-		res.send(author)
-	} catch (err) {
-		res.status(500).send({ message: "Something went wrong" })
-	}
-})
+router.post('/', store)
 
 // POST /authors/:authorId/books
-router.post('/:authorId/books', async (req, res) => {
-	try {
-		const result = await prisma.author.update({
-			where: {
-				id: Number(req.params.authorId),
-			},
-			data: {
-				books: {
-					connect: {
-						id: req.body.bookId,
-					}
-				}
-			},
-			include: {
-				books: true,
-			}
-		})
-		res.status(201).send(result)
-	} catch (err) {
-		res.status(500).send({ message: "Something went wrong" })
-	}
-})
+router.post('/:authorId/books', addBook)
 
 export default router
