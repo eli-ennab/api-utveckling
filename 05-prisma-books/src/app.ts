acad76f14335fd0f916060662e444fc93b6421ad
Eli Ennab <elinaennab@gmail.com>
2023-01-31 10:10:47 +0100

    Installed cors and implemented in app.ts.

--- 05-prisma-books/src/app.ts
+++ 05-prisma-books/src/app.ts
@@ -1,9 +1,11 @@
+import cors from 'cors'
 import express from "express"
 import prisma from "./prisma" 	// importing the prisma instance we created
 import morgan from "morgan"
 import routes from "./routes"
 
 const app = express()
+app.use(cors())
 app.use(express.json())			// middleware
 app.use(morgan('dev'))			// middleware
 
