d634aece34d3891b3f2dffeb43d2582aaf2f0a26
Eli Ennab <elinaennab@gmail.com>
2023-02-01 14:54:38 +0100

    Hash plaintext-password using bcrypt and create user in database.

--- 05-prisma-books/src/controllers/register_controller.ts
+++ 05-prisma-books/src/controllers/register_controller.ts
@@ -1,9 +1,12 @@
-// Register controller
+// Register Controller
+
+import bcrypt from 'bcrypt'
 import { Request, Response } from 'express'
 import { matchedData, validationResult } from 'express-validator'
 import prisma from '../prisma'
 
-// Get all publishers
+// Register a new user
+
 export const register = async (req: Request, res: Response) => {
 	// Check for any validation errors
 	const validationErrors = validationResult(req)
@@ -14,13 +17,32 @@ export const register = async (req: Request, res: Response) => {
 		})
 	}
 
-	// // Get only the validated data from the request
-	// const validatedData = matchedData(req)
+	// Get only the validated data from the request
+	const validatedData = matchedData(req)
+	console.log("validatedData:", validatedData)
 
-	// Calculate a hash and salt for the password
+	// Calculate a hash + salt for the password
+	const hashedPassword = await bcrypt.hash(validatedData.password, Number(process.env.SALT_ROUNDS) || 10)
+	console.log("Hashed password:", hashedPassword)
+
+	// Replace password with hashed password
+	validatedData.password = hashedPassword
 
 	// Store the user in the database
+	try {
+		const user = await prisma.user.create({
+			data: {
+				name: validatedData.name,
+				email: validatedData.email,
+				password: validatedData.password,
+			},
+		})
 
-	// Respond with 201 Created + status success
-	res.status(201).send({ "status": "success", "data": req.body })
+		// Respond with 201 Created + status success
+		res.status(201).send({ status: "success", data: user })
+
+	} catch (err) {
+		return res.status(500).send({ status: "error", message: "Could not create user in database" })
+	}
 }
+
