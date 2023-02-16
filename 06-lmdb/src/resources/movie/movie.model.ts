a49a8efd5b16a4bc08fe3a7aa049eda3266aa063
Eli Ennab <elinaennab@gmail.com>
2023-02-16 10:08:20 +0100

    Validation to Movie Model.

--- 06-lmdb/src/resources/movie/movie.model.ts
+++ 06-lmdb/src/resources/movie/movie.model.ts
@@ -4,12 +4,38 @@ export interface IMovie extends Document {
 	title: string,
 	runtime?: number,
 	releaseYear?: number,
+	genre?: string,
+	watched?: Date,
 }
 
 const MovieSchema: Schema = new Schema<IMovie>({
-	title: { type: String, required: true },
-	runtime: { type: Number, min: 1 },
-	releaseYear: { type: Number, min: 1888 },
+	title: {
+		type: String, 		// validation
+		required: true, 	// validation
+		trim: true, 		// sanitazition
+		minlength: 3,
+		unique: true,
+	},
+	runtime: {
+		type: Number,
+		min: 1,
+		default: null,
+	},
+	releaseYear: {
+		type: Number,
+		min: 1888,
+		max: new Date().getFullYear(),
+	},
+	genre: {
+		type: String,
+		lowercase: true,
+		enum: ["action", "sci-fi", "bromance", "realism"],
+	},
+	watched: {
+		type: Date,
+		default: Date.now(),
+		max: Date.now(),
+	}
 })
 
 export const Movie = model<IMovie>('Movie', MovieSchema)
