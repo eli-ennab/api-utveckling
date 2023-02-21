7969334e44a0e19d40987df479015baed5707547
Eli Ennab <elinaennab@gmail.com>
2023-02-21 15:34:48 +0100

    Teacher's solution to realtime send and recieve messages.

--- 07-chat/frontend/src/main.ts
+++ 07-chat/frontend/src/main.ts
@@ -10,7 +10,7 @@ const SOCKET_HOST = import.meta.env.VITE_APP_SOCKET_HOST
 
 const messageEl = document.querySelector('#message') as HTMLInputElement
 const messageFormEl = document.querySelector('#message-form') as HTMLFormElement
-const messagesEl = document.querySelector('#messages') as HTMLDivElement
+const messagesEl = document.querySelector('#messages') as HTMLUListElement
 
 // Connect to Socket.IO server
 const socket: Socket<ServerToClientEvents, ClientToServerEvents> = io(SOCKET_HOST)
@@ -30,10 +30,35 @@ socket.on('hello', () => {
 	console.log('The server says hello')
 })
 
+// Add new chat messages to the chat
+const addMessageToChat = (message: ChatMessageData, ownMessage = false) => {
+	// messagesEl.innerHTML += `<li class="message ${author?"own-message":""}">${message.content}</li>`
+
+	// Create a new LI element
+	const messageEl = document.createElement('li')
+
+	// Set class of LI to 'message'
+	messageEl.classList.add('message')
+
+	// If the message is from the user, add the class own-message
+	if (ownMessage) {
+		messageEl.classList.add('own-message')
+	}
+
+	// Set the text content of the LI element to the message
+	messageEl.textContent = message.content
+
+	// Append the LI element to the messages element
+	messagesEl.appendChild(messageEl)
+
+	// Scroll to the bottom of the messages list
+	messageEl.scrollIntoView({ behavior: 'smooth' })
+}
+
 // Listen for new chat messages
 socket.on('chatMessage', (message) => {
 	console.log('New chat message:', message)
-	// addMessageToChat()
+	addMessageToChat(message)
 })
 
 // Send a message to the server when form is submitted
@@ -52,7 +77,7 @@ messageFormEl.addEventListener('submit', e => {
 	// Send (emit) the message to the server
 	socket.emit('sendChatMessage', message)
 
-	// addMessageToChat(message, true)
+	addMessageToChat(message, true)
 
 	console.log("Emitted 'sendChatMessage' event to server", message)
 
